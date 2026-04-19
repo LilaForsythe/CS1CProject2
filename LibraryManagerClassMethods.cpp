893e00fd9906315fcd17e45756a4d6b1a7665947
Michelle <michelle2006a@gmail.com>
2026-04-18 22:19:04 -0700

    Implemented manager class and integrated with main
    Implemented LibraryManagerClass to manage all media objects using polymorphism.
    Added functionality for adding/removing items, searching by name/ID, checking items in and out, and displaying availability.
    Integrated manager into main and tested with input file.
    Need to fix formatting for available media.

---
+++ LibraryManagerClassMethods.cpp
@@ -0,0 +1,318 @@
+#include "LibraryManagerClassHeader.h"
+
+LibraryManager::LibraryManager()
+{
+}
+
+LibraryManager::~LibraryManager()
+{
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        delete mediaList[i];
+    }
+    mediaList.clear();
+}
+
+void LibraryManager::addItem(BaseMedia* item)
+{
+    mediaList.push_back(item);
+}
+
+void LibraryManager::removeItemByID(int id)
+{
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        if (mediaList[i]->getID() == id)
+        {
+            delete mediaList[i];
+            mediaList.erase(mediaList.begin() + i);
+            return;
+        }
+    }
+
+    throw "Error: item not found.";
+}
+
+BaseMedia* LibraryManager::searchByID(int id)
+{
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        if (mediaList[i]->getID() == id)
+        {
+            return mediaList[i];
+        }
+    }
+
+    return nullptr;
+}
+
+BaseMedia* LibraryManager::searchByName(const string& name)
+{
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        if (mediaList[i]->getName() == name)
+        {
+            return mediaList[i];
+        }
+    }
+
+    return nullptr;
+}
+
+void LibraryManager::checkOutItem(int id)
+{
+    BaseMedia* item = searchByID(id);
+
+    if (item == nullptr)
+    {
+        throw "Error: item not found.";
+    }
+
+    if (!item->isAvailable())
+    {
+        throw "Error: item already checked out.";
+    }
+
+    item->setAvailable(false);
+}
+
+void LibraryManager::returnItem(int id)
+{
+    BaseMedia* item = searchByID(id);
+
+    if (item == nullptr)
+    {
+        throw "Error: item not found.";
+    }
+
+    if (item->isAvailable())
+    {
+        throw "Error: item was not checked out.";
+    }
+
+    item->setAvailable(true);
+}
+
+bool LibraryManager::checkAvailability(int id)
+{
+    BaseMedia* item = searchByID(id);
+
+    if (item == nullptr)
+    {
+        throw "Error: item not found.";
+    }
+
+    return item->isAvailable();
+}
+
+void LibraryManager::displayAllMedia()
+{
+    cout << "\n**************** ALL MEDIA ****************\n";
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        mediaList[i]->displayMediaInfo();
+    }
+}
+
+void LibraryManager::displayAvailableMedia()
+{
+    cout << "\n**************** AVAILABLE MEDIA ****************\n";
+    
+    cout << endl << left << setw(9) << "Type" << " | " << setw(20) << "Name" << "| " << setw(10) << "Length";
+    cout << "| " << "Date" << " | " << "ID" << " | " << setw(15) << "Author";
+    cout << "| " << setw(10) << "Genre" << "| " << setw(12) << "Availability" << endl;
+    cout << setfill('-') << setw(105) << "" << endl << setfill(' ');
+
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        if (mediaList[i]->isAvailable())
+        {
+            mediaList[i]->displayMediaInfo();
+        }
+    }
+}
+
+void LibraryManager::displayCheckedOutMedia()
+{
+    cout << "\n**************** CHECKED OUT MEDIA ****************\n";
+   
+    cout << endl << left << setw(9) << "Type" << " | " << setw(20) << "Name" << "| " << setw(10) << "Length";
+    cout << "| " << "Date" << " | " << "ID" << " | " << setw(15) << "Author";
+    cout << "| " << setw(10) << "Genre" << "| " << setw(12) << "Availability" << endl;
+    cout << setfill('-') << setw(105) << "" << endl << setfill(' ');
+
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        if (!mediaList[i]->isAvailable())
+        {
+            mediaList[i]->displayMediaInfo();
+        }
+    }
+
+}
+
+void LibraryManager::displayNumberOfEachType()
+{
+    int bookCount = 0;
+    int audiobookCount = 0;
+    int magazineCount = 0;
+    int movieCount = 0;
+    int albumCount = 0;
+
+    for (int i = 0; i < mediaList.size(); i++)
+    {
+        switch (mediaList[i]->getType())
+        {
+        case Book:
+            bookCount++;
+            break;
+        case Audiobook:
+            audiobookCount++;
+            break;
+        case Magazine:
+            magazineCount++;
+            break;
+        case Movie:
+            movieCount++;
+            break;
+        case Album:
+            albumCount++;
+            break;
+        }
+    }
+
+    cout << "\n**************** MEDIA COUNTS ****************\n";
+    cout << "Books: " << bookCount << endl;
+    cout << "Audiobooks: " << audiobookCount << endl;
+    cout << "Magazines: " << magazineCount << endl;
+    cout << "Movies: " << movieCount << endl;
+    cout << "Albums: " << albumCount << endl;
+}
+
+string LibraryManager::trim(const string& str)
+{
+    int start = 0;
+    while (start < str.length() && str[start] == ' ')
+    {
+        start++;
+    }
+
+    int end = str.length() - 1;
+    while (end >= 0 && str[end] == ' ')
+    {
+        end--;
+    }
+
+    if (start > end)
+    {
+        return "";
+    }
+
+    return str.substr(start, end - start + 1);
+}
+
+void LibraryManager::loadFromFile(const string& fileName)
+{
+    ifstream fin(fileName);
+
+    if (!fin)
+    {
+        throw "Error: could not open input file.";
+    }
+
+    string line;
+
+    while (getline(fin, line))
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+
+        vector<string> fields;
+        string current = "";
+
+        for (int i = 0; i < line.length(); i++)
+        {
+            if (line[i] == ',')
+            {
+                fields.push_back(trim(current));
+                current = "";
+            }
+            else
+            {
+                current += line[i];
+            }
+        }
+        fields.push_back(trim(current));
+
+        string type = fields[0];
+
+        if (type == "Book" && fields.size() >= 8)
+        {
+            BookClass* newBook = new BookClass(
+                Book,
+                fields[1],
+                stod(fields[2]),
+                stoi(fields[3]),
+                stoi(fields[4]),
+                fields[5],
+                fields[6]
+            );
+
+            newBook->setAvailable(fields[7] == "available");
+            addItem(newBook);
+        }
+        else if (type == "Audiobook" && fields.size() >= 9)
+        {
+            AudiobookClass* newAudiobook = new AudiobookClass(
+                Audiobook,
+                fields[1],
+                stod(fields[2]),
+                stoi(fields[3]),
+                stoi(fields[4]),
+                fields[5],
+                fields[6],
+                fields[7]
+            );
+
+            newAudiobook->setAvailable(fields[8] == "available");
+            addItem(newAudiobook);
+        }
+        else if (type == "Magazine" && fields.size() >= 9)
+        {
+            MagazineClass* newMagazine = new MagazineClass(
+                Magazine,
+                fields[1],
+                stod(fields[2]),
+                stoi(fields[3]),
+                stoi(fields[4]),
+                fields[5],
+                stoi(fields[6]),
+                fields[7]
+            );
+
+            newMagazine->setAvailable(fields[8] == "available");
+            addItem(newMagazine);
+        }
+        else if (type == "Movie" && fields.size() >= 10)
+        {
+            MovieClass* newMovie = new MovieClass(
+                Movie,
+                fields[1],
+                stod(fields[2]),
+                stoi(fields[3]),
+                stoi(fields[4]),
+                fields[6],   // actor1
+                fields[7],   // actor2
+                fields[5],   // director
+                fields[8]    // genre
+            );
+
+            newMovie->setAvailable(fields[9] == "available");
+            addItem(newMovie);
+        }
+    }
+
+    fin.close();
+}
--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include <string>
 #include <fstream>
 #include <vector>
+
 #include "BaseMediaClassHeader.h"
 #include "AudiobookClassHeader.h"
 #include "MagazineClassHeader.h"
 #include "MovieClassHeader.h"
+#include "BookClassHeader.h"
+#include "LibraryManagerClassHeader.h"
 using namespace std;
 
 int main()
@@ -29,18 +32,59 @@ int main()
     string director;
 
     //vectors holding pieces of media
+    vector<BookClass> books;
     vector<AudiobookClass> audiobooks;
     vector<MagazineClass> magazines;
     vector<MovieClass> movies;
+
+	// manager class object - (change comment description)
+	LibraryManager manager;
+
 
     while (!fin.eof())
     {
         //reading in type
         getline(fin, type, ',');
-        //reading in the rest
+
+        // added the book section to already created main
         if (type == "Book")
         {
             option = Book;
+            fin.get();
+            getline(fin, name, ',');
+            fin.get();
+            fin >> length;
+            fin.get();
+            fin.get();
+            fin >> date;
+            fin.get();
+            fin.get();
+            fin >> id;
+            fin.get();
+            fin.get();
+            getline(fin, author, ',');
+            fin.get();
+            getline(fin, genre, ',');
+            fin.get();
+            getline(fin, available, '\n');
+
+            BookClass newBook(option, name, length, date, id, author, genre);
+
+            if (available == "available")
+            {
+                newBook.setAvailable(true);
+            }
+            else if (available == "checked out")
+            {
+                newBook.setAvailable(false);
+            }
+
+			// add book to manager polymorphically
+            books.push_back(newBook);
+            BookClass* bookPtr = new BookClass(option, name, length, date, id, author, genre);
+            bookPtr->setAvailable(newBook.isAvailable());
+            manager.addItem(bookPtr);
+
         }
         else if (type == "Audiobook")
         {
@@ -76,6 +120,12 @@ int main()
             }
 
             audiobooks.push_back(newAudiobook);
+
+            // add to manager polymorphically
+            AudiobookClass* audiobookPtr = new AudiobookClass(option, name, length, date, id, author, narrator, genre);
+            audiobookPtr->setAvailable(newAudiobook.isAvailable());
+            manager.addItem(audiobookPtr);
+
         }
         else if (type == "Magazine")
         {
@@ -113,6 +163,11 @@ int main()
             }
 
             magazines.push_back(newMagazine);
+
+            // add to manager polymorphically
+            MagazineClass* magazinePtr = new MagazineClass(option, name, length, date, id, author, issue, genre);
+            magazinePtr->setAvailable(newMagazine.isAvailable());
+            manager.addItem(magazinePtr);
         }
         else if (type == "Movie")
         {
@@ -151,54 +206,104 @@ int main()
             }
 
             movies.push_back(newMovie);
+
+            // add to manager polymorphically
+            MovieClass* moviePtr = new MovieClass(option, name, length, date, id, actor1, actor2, director, genre);
+            moviePtr->setAvailable(newMovie.isAvailable());
+            manager.addItem(moviePtr);
         }
         else if (type == "Album")
         {
             option = Album;
+            getline(fin, available, '\n');
+        }
+        else
+        {
+            getline(fin, available, '\n');
         }
     }
     fin.close();
 
-    //eventually the manager class will do this
+    //display all books :)
+    cout << endl << left << setw(9) << "Type" << " | " << setw(20) << "Name" << "| " << setw(10) << "Length";
+    cout << "| " << "Date" << " | " << "ID" << " | " << setw(15) << "Author";
+    cout << "| " << setw(10) << "Genre" << "| " << setw(12) << "Availability" << endl;
+    cout << setfill('-') << setw(105) << "" << endl << setfill(' ');
+
+    for (int i = 0; i < books.size(); i++)
+    {
+        books[i].displayMediaInfo();
+    }
+
     //display all audiobooks
-    //displaying the header
-    cout << endl << left << setw(9) << "Type" << " | " << setw(20) << "Name" << "| " <<setw(22) << "Length";
+    cout << endl << left << setw(9) << "Type" << " | " << setw(20) << "Name" << "| " << setw(22) << "Length";
     cout << "| " << "Date" << " | " << "ID" << " | " << setw(15) << "Author" << "| " << setw(15) << "Narrator";
     cout << "| " << setw(10) << "Genre" << "| " << setw(12) << "Availability" << endl;
     cout << setfill('-') << setw(129) << "" << endl << setfill(' ');
 
-    //displaying the media
     for (int i = 0; i < audiobooks.size(); i++)
     {
         audiobooks[i].displayMediaInfo();
     }
 
     //display all magazines
-    //displaying the header
-    cout << endl << left << setw(8) << "Type" << " | " << setw(30) << "Name" << "| " <<setw(10) << "Length";
+    cout << endl << left << setw(8) << "Type" << " | " << setw(30) << "Name" << "| " << setw(10) << "Length";
     cout << "| " << "Date" << " | " << "ID" << " | " << setw(20) << "Author" << "| " << "Issue";
     cout << " | " << setw(10) << "Genre" << "| " << setw(12) << "Availability" << endl;
     cout << setfill('-') << setw(122) << "" << endl << setfill(' ');
 
-    //displaying the media
     for (int i = 0; i < magazines.size(); i++)
     {
         magazines[i].displayMediaInfo();
     }
 
     //display all movies
-    //displaying the header
-    cout << endl << left << setw(6) << "Type" << "| " << setw(20) << "Name" << "| " <<setw(21) << "Length";
+    cout << endl << left << setw(6) << "Type" << "| " << setw(20) << "Name" << "| " << setw(21) << "Length";
     cout << "| " << "Date" << " | " << "ID" << " | " << setw(15) << "Director" << "| " << setw(15) << "Lead 1";
     cout << "| " << setw(15) << "Lead 2" << "| " << setw(15) << "Genre" << "| " << setw(12);
     cout << "Availability" << endl;
     cout << setfill('-') << setw(147) << "" << endl << setfill(' ');
 
-    //displaying the media
     for (int i = 0; i < movies.size(); i++)
     {
         movies[i].displayMediaInfo();
     }
+
+    cout << "\n\n**************** MANAGER CLASS DEMO ****************\n";
+
+    manager.displayNumberOfEachType();
+
+    try
+    {
+        cout << "\nSearching for Dune...\n";
+        BaseMedia* found = manager.searchByName("Dune");
+
+        if (found != nullptr)
+        {
+            found->displayMediaInfo();
+        }
+        else
+        {
+            cout << "Dune not found." << endl;
+        }
+
+        cout << "\nChecking out item ID 1...\n";
+        manager.checkOutItem(1);
+
+        cout << "Returning item ID 1...\n";
+        manager.returnItem(1);
+
+        cout << "\nDisplaying available media...\n";
+        manager.displayAvailableMedia();
+
+        cout << "\nDisplaying checked out media...\n";
+        manager.displayCheckedOutMedia();
+    }
+    catch (const char* errorMessage)
+    {
+        cout << errorMessage << endl;
+    }
+
 
     return 0;
 }
