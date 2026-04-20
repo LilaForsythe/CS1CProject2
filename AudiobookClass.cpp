f0cd073ccb2f59b08ecaf625700ebc9e60186917
MarianaNunez <mariana.nunez.garcia@gmail.com>
2026-04-20 16:04:18 -0700

    Implement AudiobookClass with constructors and methods

---
+++ AudiobookClass.cpp
@@ -0,0 +1,55 @@
+
+#include "AudiobookClass.h"
+AudiobookClass::AudiobookClass()
+{
+
+}
+
+
+AudiobookClass::AudiobookClass(Type type, string name, double length, int date, int id, string author, string narrator, string genre)
+{
+    setType(type);
+    setName(name);
+    setLength(length);
+    setDate(date);
+    setID(id);
+    setAuthor(author);
+    setNarrator(narrator);
+    setGenre(genre);
+}
+AudiobookClass::~AudiobookClass()
+{
+
+}
+
+string AudiobookClass::getNarrator()
+{
+    return narrator;
+}
+void AudiobookClass::setNarrator(string narrator)
+{
+    this->narrator = narrator;
+}
+
+void AudiobookClass::displayMediaInfo()
+{
+    cout << "Audiobook " << "| " << setw(20) << this->getName() << "| ";
+
+    int hrs = 0;
+    int mins = 0;
+    hrs = this->getLength();
+    mins = (int(this->getLength() * 100)) % 100;
+
+    cout << setw(2) << hrs << " hours & " << mins << " minutes" << " | " << this->getDate() << " | ";
+    cout << setw(2) << this->getID() << " | " << setw(15) << this->getAuthor() << "| ";
+    cout << setw(15) << narrator << "| " << setw(10) << this->getGenre() << "| ";
+
+    if (isAvailable())
+    {
+        cout << "Available" << endl;
+    }
+    else
+    {
+        cout << "Checked Out" << endl;
+    }
+}
