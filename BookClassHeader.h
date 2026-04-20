b7ba428ceed0b5f0d139999419d76037d30cd85c
Gabriel Shepardson <gshepardson0@saddleback.edu>
2026-04-19 23:19:41 -0700

    Add BookClass header file with class definition

---
+++ BookClassHeader.h
@@ -0,0 +1,23 @@
+#ifndef CS1CPROJECT2_BOOKCLASS_H
+#define CS1CPROJECT2_BOOKCLASS_H
+
+#include "BaseMediaClassHeader.h"
+#include "AuthorInterface.h"
+
+class BookClass : public BaseMedia,
+				  public AuthorInterface
+{
+	public:
+		BookClass();
+		BookClass(Type type, string name, double length, int date, int id, string author, string genre);
+		virtual ~BookClass();
+
+		virtual string getAuthor();
+		virtual void setAuthor(string author);
+		virtual void displayMediaInfo();
+
+	private:
+		string author;
+};
+
+#endif
