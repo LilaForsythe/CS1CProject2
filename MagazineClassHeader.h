2597b17459af90e913302a10f14bcb5138f84500
MarianaNunez <mariana.nunez.garcia@gmail.com>
2026-04-20 15:57:37 -0700

    Add MagazineClass header file with class definition

---
+++ MagazineClassHeader.h
@@ -0,0 +1,31 @@
+
+#ifndef CS1CPROJECT2_MAGAZINECLASS_H
+#define CS1CPROJECT2_MAGAZINECLASS_H
+
+#include "BaseMediaClass.h"
+
+class MagazineClass: public BaseMedia
+{
+public:
+    MagazineClass();
+    MagazineClass(Type type, string name, double length, int date, int id, string author, int issue, string genre);
+    ~MagazineClass();
+
+    int getIssue();
+    void setIssue(int issue);
+
+    virtual void displayMediaInfo();
+private:
+    int issue = 0;
+    Type type;
+    string name = "";
+    double length = 0.0;
+    int date = 0;
+    int ID = 0;
+    string author = "";
+    string genre = "";
+    bool available = false;
+};
+
+
+#endif //CS1CPROJECT2_MAGAZINECLASS_H
