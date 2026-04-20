ca8e33a6b8aa31adb9a019a1ba0b306a10b95955
MarianaNunez <mariana.nunez.garcia@gmail.com>
2026-04-20 15:56:16 -0700

    Add MovieClass header file with member functions

---
+++ MovieClassHeader.h
@@ -0,0 +1,38 @@
+
+
+#ifndef CS1CPROJECT2_MOVIECLASS_H
+#define CS1CPROJECT2_MOVIECLASS_H
+
+
+#include "BaseMediaClass.h"
+
+class MovieClass: public BaseMedia
+{
+public:
+    MovieClass();
+    MovieClass(Type type, string name, double length, int date, int id, string actor1, string actor2, string director, string genre);
+    ~MovieClass();
+
+    string getDirector();
+    void setDirector(string director);
+    string getActor1();
+    void setActor1(string actor1);
+    string getActor2();
+    void setActor2(string actor2);
+
+    virtual void displayMediaInfo();
+private:
+    string director = "";
+    Type type;
+    string name = "";
+    double length = 0.0;
+    int date = 0;
+    int ID = 0;
+    string actor1 = "";
+    string actor2 = "";
+    string genre = "";
+    bool available = false;
+};
+
+
+#endif //CS1CPROJECT2_MOVIECLASS_H
